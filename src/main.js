df703aaeecf7e4b42336c8d510880ea2e6827379
Eder <ejcalderilla@gmail.com>
2022-08-29 09:09:59 -0700

    appBar

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import ResponsiveAppBar from './appBar'
 import { Orders } from './orders'
 import { OrderDetails } from './order_details'
 
 export const Main = () => {
   return (
+    <>    
+    <ResponsiveAppBar />
     <Routes>
       <Route path='/' element={<Orders />} />
       <Route path='/Orders' element={<Orders />} />
       <Route path='/OrderDetail/:Id' element={<OrderDetails />} />
     </Routes>
+    </>
   )
 }
